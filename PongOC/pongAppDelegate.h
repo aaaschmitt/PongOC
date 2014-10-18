3881aecaf4e01e5028387b3b6cb670cfeb31b5cb
aaaschmitt <aschmitt@berkeley.edu>
2014-10-18 11:45:08 -0700

    initial push

---
+++ PongOC/pongAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  pongAppDelegate.h
+//  PongOC
+//
+//  Created by Andrew Schmitt on 10/18/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pongAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PongOC/pongViewController.h
@@ -0,0 +1,14 @@
+//
+//  pongViewController.h
+//  PongOC
+//
+
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface pongViewController : UIViewController
+
+@end
